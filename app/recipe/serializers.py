696730cc3aa08ff97b46b4939c699c6b16579477
Chris Gibson <chris.gibson83@outlook.com>
2023-12-17 09:52:41 +1100

    add ingredient testing

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,7 +6,17 @@ from rest_framework import serializers
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients"""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -21,11 +31,24 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """serializer for recipes """
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags',
+            'ingredients',
+                  ]
         read_only_fields = ['id']
+
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """handle getting or creating ingredients as needed"""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, create = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
 
     def _get_or_create_tags(self, tags, recipe):
         """handle getting or creating tags as needed"""
@@ -40,8 +63,10 @@ class RecipeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """create a recipe"""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
